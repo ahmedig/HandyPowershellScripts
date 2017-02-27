e18db28fefab903dd3b2e55b06d1485986919e34
Ahmed AlSayed <ahmedsayedig@gmail.com>
2017-02-27 11:20:04 +1100

    Create TestConnectionString.ps1

---
+++ TestConnectionString.ps1
@@ -0,0 +1,26 @@
+function Test-SQLConnection
+{    
+    [OutputType([bool])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position=0)]
+        $ConnectionString
+    )
+    try
+    {
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $ConnectionString;
+        $sqlConnection.Open();
+        $sqlConnection.Close();
+
+        return $true;
+    }
+    catch
+    {
+        return $false;
+    }
+}
+
+## Test it using this command:
+## Test-SQLConnection "Data Source=localhost;database=myDB;User ID=myUser;Password=myPassword;"
